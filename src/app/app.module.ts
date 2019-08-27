2b00de00bf62caed6a2334597433306ba95e6e26
Aron Szocs <Aron_Szocs@epam.com>
2019-08-27 10:29:50 +0200

    wired in skeleton files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
+
+// Pages
+import { HomePage } from './pages/home-page/home.page';
+
+// Services
+import { LoadingScreenService } from './services/loading-screen-service/loading-screen.service';
+import { RouterService } from './services/router-service/router.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // Components
+    AppComponent,
+    NavComponent,
+    LoadingScreenComponent,
+    // Pages
+    HomePage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '**', component: HomePage }
+    ], { useHash: true })
   ],
-  providers: [],
+  providers: [
+    LoadingScreenService,
+    RouterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
